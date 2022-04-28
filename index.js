cf059bfc53b9880229945bca4bf687b39c738700
Shadowlaq <38447226+shadowlaq@users.noreply.github.com>
2022-04-27 22:59:19 -0700

    nav header development draft

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+//Trisplit
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const middle = document.querySelector(".middle");
 const container = document.querySelector(".container");
+//Header/Nav
+const headNav = document.querySelector(".built-with")
+const navBar = document.querySelector(".nav-bar")
 
-//Mouseover/hover class toggling
+//Mouseover/hover class toggling for nav
+headNav.addEventListener("mouseenter", () => {
+    navBar.classList.remove("nav-up");
+  });
+  
+  navBar.addEventListener("mouseleave", () => {
+    navBar.classList.add("nav-up");
+});
 
+//Mouseover/hover class toggling for tri-split area
 //LEFT
 left.addEventListener("mouseenter", () => {
   container.classList.add("hover-left");
